edcfd13668581eb31476875525f00e77c520980c
SaeruHikari <39457738+misakaXunyan@users.noreply.github.com>
2020-04-03 00:19:05 +0800

    Remove visual-studio out folder

--- out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Coding/SakuraAutoCoder
-# Build directory: D:/Coding/SakuraAutoCoder/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
